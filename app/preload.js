4299e77cc290e71092cefe2d0a3584ef519fd950
kei-g <km.8k6ce+github@gmail.com>
2024-05-22 16:03:30 +0900

    :goal_net: Attemp to catch unhandled rejections in the webview
    Signed-off-by: kei-g <km.8k6ce+github@gmail.com>

--- app/preload.js
+++ app/preload.js
@@ -1,44 +1,30 @@
 const { ipcRenderer } = require('electron')
 
-const accumulateTextContents = nodes => {
-  const list = []
-  nodes.forEach(
-    element => element.childElementCount === 0 && element.childNodes.forEach(
-      node => list.push(node.textContent)
-    )
-  )
-  return list
-}
+class Preload {
+  #caught = []
+  #window = undefined
 
-ipcRenderer.on(
-  'download',
-  async (_, { channel, url }) => {
-    const res = await window.fetch(url)
-    const { status, statusText } = res
-    const result = { status, statusText, url }
-    if (status === 200) {
-      const blob = await res.blob()
-      result.data = new Uint8Array(await blob.arrayBuffer())
-    }
-    ipcRenderer.sendToHost(channel, result)
+  #catchRejection(event) {
+    this.#caught.push(event)
+    event.preventDefault()
+    console.error(`[preload] unhandled rejection: ${event.message}`)
   }
-)
 
-ipcRenderer.on(
-  'lookup-images',
-  (_, ...args) => {
-    const failure = document.querySelector('div#ScriptLoadFailure:has(>form)')
+  #handleLookupImagesRequest(_, ...args) {
+    if (this.#caught.length)
+      return ipcRenderer.sendToHost('images-found', { abort: true, error: this.#caught.splice(0).map(c => c.message).join(',') }, ...args)
+    const failure = this.#querySelector('div#ScriptLoadFailure:has(>form)')
     if (failure)
       return ipcRenderer.sendToHost('images-found', { abort: true, error: 'スクリプト読み込みエラー' }, ...args)
-    const errorDetail = document.querySelector('[data-testid="error-detail"]')
+    const errorDetail = this.#querySelector('[data-testid="error-detail"]')
     if (errorDetail) {
       const error = accumulateTextContents(errorDetail.querySelectorAll('div>span:first-child span')).join(',')
       return ipcRenderer.sendToHost('images-found', { error }, ...args)
     }
-    const toast = document.querySelector('[data-testid="toast"]')
+    const toast = this.#querySelector('[data-testid="toast"]')
     if (toast?.innerText === 'そのポストは削除されました。')
       return ipcRenderer.sendToHost('images-found', { error: toast.innerText }, ...args)
-    const main = document.querySelector('main[role="main"]')
+    const main = this.#querySelector('main[role="main"]')
     if (!main)
       return ipcRenderer.sendToHost('images-found', { error: '<main>タグが無い' }, ...args)
     const progressbars = main.querySelectorAll('div[aria-label][role="progressbar"]')
@@ -70,6 +56,50 @@ ipcRenderer.on(
     }
     ipcRenderer.sendToHost('images-found', images, ...args)
   }
+
+  #querySelector(selectors) {
+    return this.#window.document.querySelector(selectors)
+  }
+
+  #querySelectorAll(selectors) {
+    return this.#window.document.querySelectorAll(selectors)
+  }
+
+  constructor() {
+    ipcRenderer.on('lookup-images', this.#handleLookupImagesRequest.bind(this))
+  }
+
+  attachTo(window) {
+    this.#window = window
+    window.addEventListener('unhandledrejection', this.#catchRejection.bind(this))
+  }
+}
+
+const accumulateTextContents = nodes => {
+  const list = []
+  nodes.forEach(
+    element => element.childElementCount === 0 && element.childNodes.forEach(
+      node => list.push(node.textContent)
+    )
+  )
+  return list
+}
+
+ipcRenderer.on(
+  'download',
+  async (_, { channel, url }) => {
+    const res = await window.fetch(url)
+    const { status, statusText } = res
+    const result = { status, statusText, url }
+    if (status === 200) {
+      const blob = await res.blob()
+      result.data = new Uint8Array(await blob.arrayBuffer())
+    }
+    ipcRenderer.sendToHost(channel, result)
+  }
 )
 
 const twitterImageUrlRE = /^(?<prefix>https:\/\/pbs\.twimg\.com\/media\/)(?<id>[^\?]+)\?format=(?<format>[^&]+)&?.*$/g
+
+const preload = new Preload()
+preload.attachTo(window)
