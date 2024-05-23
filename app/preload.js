9402af2a251af0cbddcceff74b8eccb3b9ee633f
kei-g <km.8k6ce+github@gmail.com>
2024-05-23 09:25:40 +0900

    :recycle: Separate #handleImageElement from #handleLookupImagesRequest
    Signed-off-by: kei-g <km.8k6ce+github@gmail.com>

--- app/preload.js
+++ app/preload.js
@@ -10,6 +10,20 @@ class Preload {
     console.error(`[preload] unhandled rejection: ${event.message}`)
   }
 
+  #handleImageElement(images, element) {
+    const src = element.getAttribute('src')
+    for (const m of src.matchAll(twitterImageUrlRE)) {
+      const { format, id, prefix } = m.groups
+      const matched = m[0]
+      const name = `${id}.${format}`
+      const url = `${prefix}${name}:large`
+      if (name in images)
+        continue
+      images[name] = { matched, url }
+      images.size++
+    }
+  }
+
   #handleLookupImagesRequest(_, ...args) {
     if (this.#caught.length)
       return ipcRenderer.sendToHost('images-found', { abort: true, error: this.#caught.splice(0).map(c => c.message).join(',') }, ...args)
@@ -26,20 +40,7 @@ class Preload {
         if (!primaries.length)
           return ipcRenderer.sendToHost('images-found', { error: 'primaryColumnが無い', retryLater: true }, ...args)
         const ctx = {}
-        primaries.forEach(
-          primaryColumn => {
-            if (ctx.error)
-              return
-            const cells = primaryColumn.querySelectorAll('div[data-testid="cellInnerDiv"]')
-            if (!cells.length) {
-              const progressbars = primaryColumn.querySelectorAll('div[role="progressbar"]:has(>div>svg>circle)')
-              if (progressbars.length) {
-                ctx.error = progressbars.item(0).ariaLabel
-                ctx.retryLater = true
-              }
-            }
-          }
-        )
+        primaries.forEach(this.#handlePrimaryColumn.bind(this, ctx))
         if (ctx.error)
           return ipcRenderer.sendToHost('images-found', ctx, ...args)
         break
@@ -66,50 +67,49 @@ class Preload {
         const error = accumulateTextContents(notices.item(0).parentElement.children.item(0).querySelectorAll('span')).join(',')
         return ipcRenderer.sendToHost('images-found', { error }, ...args)
       }
-      articles.item(0).querySelectorAll('[src^="https://pbs.twimg.com/media/"]').forEach(
-        element => {
-          const src = element.getAttribute('src')
-          for (const m of src.matchAll(twitterImageUrlRE)) {
-            const { format, id, prefix } = m.groups
-            const matched = m[0]
-            const name = `${id}.${format}`
-            const url = `${prefix}${name}:large`
-            if (name in images)
-              continue
-            images[name] = { matched, url }
-            images.size++
-          }
-        }
-      )
-      articles.item(0).querySelectorAll('div[data-testid="tweetPhoto"] div[data-testid="videoComponent"] video').forEach(
-        video => {
-          const [label, poster, src, type] = ['aria-label', 'poster', 'src', 'type'].map(video.getAttribute.bind(video))
-          const m = src.match(/^(?<prefix>https:\/\/video\.twimg\.com\/tweet_video\/)(?<id>[^.]+)\.(?<format>.+)$/)
-          if (m) {
-            const { format, id, prefix } = m.groups
-            const matched = m[0]
-            const name = `${id}.${format}`
-            const url = matched
-            if (!(name in images)) {
-              images[name] = { label, matched, poster, prefix, type, url }
-              images.size++
-            }
-          }
-          const t = poster?.match(/^(?<prefix>https:\/\/pbs\.twimg\.com\/[^/]+)\/(?<id>[^.]+)\.(?<format>.+)$/)
-          if (t) {
-            const { format, id, prefix } = t.groups
-            const matched = t[0]
-            const name = `${id}.${format}`
-            const url = matched
-            if (!(name in images)) {
-              images[name] = { label, matched, poster, prefix, url }
-              images.size++
-            }
-          }
-        }
-      )
+      articles.item(0).querySelectorAll('[src^="https://pbs.twimg.com/media/"]').forEach(this.#handleImageElement.bind(this, images))
+      articles.item(0).querySelectorAll('div[data-testid="tweetPhoto"] div[data-testid="videoComponent"] video').forEach(this.#handleVideoElement.bind(this, images))
     }
     ipcRenderer.sendToHost('images-found', images, ...args)
+  }
+
+  #handlePrimaryColumn(ctx, primaryColumn) {
+    if (!ctx.error) {
+      const cells = primaryColumn.querySelectorAll('div[data-testid="cellInnerDiv"]')
+      if (!cells.length) {
+        const progressbars = primaryColumn.querySelectorAll('div[role="progressbar"]:has(>div>svg>circle)')
+        if (progressbars.length) {
+          ctx.error = progressbars.item(0).ariaLabel
+          ctx.retryLater = true
+        }
+      }
+    }
+  }
+
+  #handleVideoElement(images, video) {
+    const [label, poster, src, type] = ['aria-label', 'poster', 'src', 'type'].map(video.getAttribute.bind(video))
+    const m = src.match(/^(?<prefix>https:\/\/video\.twimg\.com\/tweet_video\/)(?<id>[^.]+)\.(?<format>.+)$/)
+    if (m) {
+      const { format, id, prefix } = m.groups
+      const matched = m[0]
+      const name = `${id}.${format}`
+      const url = matched
+      if (!(name in images)) {
+        images[name] = { label, matched, poster, prefix, type, url }
+        images.size++
+      }
+    }
+    const t = poster?.match(/^(?<prefix>https:\/\/pbs\.twimg\.com\/[^/]+)\/(?<id>[^.]+)\.(?<format>.+)$/)
+    if (t) {
+      const { format, id, prefix } = t.groups
+      const matched = t[0]
+      const name = `${id}.${format}`
+      const url = matched
+      if (!(name in images)) {
+        images[name] = { label, matched, poster, prefix, url }
+        images.size++
+      }
+    }
   }
 
   #querySelector(selectors) {
