ea215de9159d54258512bf77665293611baf2be9
kei-g <km.8k6ce+github@gmail.com>
2024-06-18 02:30:28 +0900

    :boom: Label pictures those have unique color index, by 9999 rather than that index
    Signed-off-by: kei-g <km.8k6ce+github@gmail.com>

--- app/categorizing.js
+++ app/categorizing.js
@@ -1,10 +1,55 @@
-window.addEventListener(
-  'DOMContentLoaded',
-  () => {
-    const progress = document.getElementById('progress')
-    ipcRenderer.on(
-      'progress',
-      (_, current, total) => progress.textContent = `${current}/${total}`
-    )
+class CategorizingController {
+  #window = undefined
+
+  #handleColorIndex(_, colorIndex, count) {
+    const { document } = this.#window
+    const colorIndices = document.getElementById('color-indices')
+    const id = `color-${colorIndex}`
+    const text = `${colorIndex.toString(10).padStart(4, '0')} - (${count})`
+    const item = document.getElementById(id)
+    if (item instanceof HTMLLIElement) {
+      item.dataset.count = count
+      item.textContent = text
+      Array.from(colorIndices.querySelectorAll('li')).sort(
+        (lhs, rhs) => parseInt(rhs.dataset.count) - parseInt(lhs.dataset.count)
+      ).forEach(
+        item => colorIndices.appendChild(item)
+      )
+    }
+    else {
+      const item = document.createElement('li')
+      item.id = id
+      item.dataset.count = count
+      item.textContent = text
+      colorIndices.appendChild(item)
+    }
   }
-)
+
+  #handleProgress(_, current, total) {
+    const { document } = this.#window
+    const progress = document.getElementById('progress')
+    progress.textContent = `${current}/${total}`
+  }
+
+  #handleStatus(_, message) {
+    const { document } = this.#window
+    const progress = document.getElementById('progress')
+    const status = document.getElementById('status')
+    progress.textContent = ''
+    status.textContent = message
+  }
+
+  #initializeComponents() {
+    ipcRenderer.on('color-indices', this.#handleColorIndex.bind(this))
+    ipcRenderer.on('progress', this.#handleProgress.bind(this))
+    ipcRenderer.on('status', this.#handleStatus.bind(this))
+  }
+
+  attachTo(window) {
+    this.#window = window
+    window.addEventListener('DOMContentLoaded', this.#initializeComponents.bind(this))
+  }
+}
+
+const controller = new CategorizingController()
+controller.attachTo(window)
