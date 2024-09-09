13b31cbc72333872e8088873cb86dd986b7f8a47
kei-g <km.8k6ce+github@gmail.com>
2024-09-09 22:57:55 +0900

    :recycle: Improve cyclomatic complexity
    Signed-off-by: kei-g <km.8k6ce+github@gmail.com>

--- tools/run.mjs
+++ tools/run.mjs
@@ -1,41 +1,75 @@
 #!/usr/bin/env node
 
-const { argv, env, stderr } = await import('node:process')
+const { argv, env } = await import('node:process')
 const { readFile } = await import('node:fs/promises')
 const { promisify } = await import('node:util')
 const { spawn } = await import('node:child_process')
 
+class StringSeparator {
+  #clearEscape(_c) {
+    this.escape = false
+  }
+
+  #handleEscape(_c) {
+    this.escape = true
+  }
+
+  #handleQuote(c) {
+    if (this.quotes.at(-1) === c) {
+      this.quotes.pop()
+      this.level--
+    }
+    else {
+      this.quotes.push(c)
+      this.level++
+    }
+  }
+
+  #handleSpace(_c) {
+    if (this.level === 0)
+      return this.#updateLast.bind(this)
+  }
+
+  #handlers = {
+    ' ': this.#handleSpace,
+    '"': this.#handleQuote,
+    '\'': this.#handleQuote,
+    '\\': this.#handleEscape,
+  }
+
+  #updateLast(index, target) {
+    const matched = target.substring(index).match(/[^ ]/)
+    if (matched)
+      this.last = index + matched.index
+  }
+
+  constructor() {
+    this.escape = false
+    this.last = 0
+    this.level = 0
+    this.quotes = []
+  }
+
+  accept(c) {
+    const handler = [this.#handlers[c], this.#clearEscape][+this.escape]
+    return handler?.apply(this, [c])
+  }
+}
+
+String.prototype.entries = function () {
+  return Array.from(new Array(this.length).keys()).map(i => [i, this[i]])
+}
+
 String.prototype.separate = function* () {
-  const ctx = {
-    escape: false,
-    last: 0,
-    level: 0,
-    quotes: [],
-  }
-  for (const [index, c] of Object.entries(this)) {
-    const i = parseInt(index, 10)
-    if (ctx.escape)
-      ctx.escape = false
-    else if (c === '\\')
-      ctx.escape = true
-    else if (['\'', '"'].includes(c)) {
-      if (ctx.quotes.at(-1) === c) {
-        ctx.quotes.pop()
-        ctx.level--
-      }
-      else {
-        ctx.quotes.push(c)
-        ctx.level++
-      }
-    }
-    else if (c === ' ' && ctx.level === 0) {
-      yield this.substring(ctx.last, i)
-      const matched = this.substring(i + 1).match(/[^ ]/)
-      if (matched)
-        ctx.last = i + 1 + matched.index
+  const separator = new StringSeparator()
+  for (const [i, c] of this.entries()) {
+    const update = separator.accept(c)
+    if (update) {
+      yield this.substring(separator.last, i)
+      update(i + 1, this)
     }
   }
-  yield this.substring(ctx.last)
+  yield this.substring(separator.last)
 }
 
 const execute = async args => {
@@ -52,27 +86,30 @@ const execute = async args => {
   }
 }
 
-const run = async args => {
+const run = async (args, scripts) => {
   for (const name of args)
     if (name in scripts) {
       const command = scripts[name]
       const separated = Array.from(command.separate())
       const index = separated.indexOf('&&')
       const rhs = index < 0 ? [] : separated.splice(index)
-      await (separated[0] === 'tools/run.mjs' ? run(separated.slice(1)) : execute(separated))
+      const procedure = [
+        execute.bind(this, separated),
+        run.bind(this, separated.slice(1), scripts),
+      ][+(separated[0] === 'tools/run.mjs')]
+      await procedure()
       rhs.shift()
       await execute(rhs)
     }
     else {
-      const separated = Array.from(
-        name.replace(
-          /^(?:'([^']+)'|"([^"]+)")$/,
-          (_, singleQuote, doubleQuote) => singleQuote ?? doubleQuote
-        ).separate()
+      const command = name.replace(
+        /^(?:'([^']+)'|"([^"]+)")$/,
+        (_, singleQuote, doubleQuote) => singleQuote ?? doubleQuote
       )
+      const separated = Array.from(command.separate())
       await execute(separated)
     }
 }
 
 const { scripts } = await readFile('package.json').then(data => JSON.parse(data.toString()))
-await run(argv.slice(2))
+await run(argv.slice(2), scripts)
